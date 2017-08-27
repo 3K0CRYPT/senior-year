0d2cd6b8b184f3eb10022d27194d5845907ef946
Michael Bartlett <bartlettmic@gmail.com>
2017-08-26 23:02:29 -0600

    a

--- Networking CSCI 471/echo_s/echo_s.cc
+++ Networking CSCI 471/echo_s/echo_s.cc
@@ -35,7 +35,7 @@ int processConnection(int sockFd) {
         DEBUG << "Closing" << std::endl;
         return 0; 
       }
-      DEBUG << "Received: " << _read << std::endl;
+      DEBUG << "Received: \"" << _read << "\"" << std::endl;
       oldRead = _read;
     }
     
