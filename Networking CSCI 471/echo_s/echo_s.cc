eb12a06aeb32275f98e2337e57502334ba250d97
Michael Bartlett <bartlettmic@gmail.com>
2017-08-26 22:47:14 -0600

    a

--- Networking CSCI 471/echo_s/echo_s.cc
+++ Networking CSCI 471/echo_s/echo_s.cc
@@ -25,7 +25,10 @@ int processConnection(int sockFd) {
     
     // Check for one of the commands
     if (_read != oldRead) {
-      std::cout << "Received: " << _read << std::endl;
+      if(_read.substr(_read.length() - 2 ) == "Ug") _read = _read.substr(0, _read.length() - 2);
+      if (_read == "QUIT") return 1;
+      if (_read == "CLOSE") return 0;
+      DEBUG << "Received: " << _read << std::endl;
       oldRead = _read;
     }
     
