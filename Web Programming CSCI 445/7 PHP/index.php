469038f9ae7c14c98a0c65c298a80445a266c248
Michael Bartlett <bartlettmic@gmail.com>
2017-11-02 13:42:52 -0600

    testphp

--- Web Programming CSCI 445/7 PHP/index.php
+++ Web Programming CSCI 445/7 PHP/index.php
@@ -14,37 +14,8 @@
     }
     </style>
 </head>
-
+<?php include 'templateHeader.php';?>
 <body>
-    <ul id="navbar">
-        <li><a href="javascript:void(0).html" class="active">Home</a></li>
-        <li><a href="aboutme/aboutme.html">About Me</a></li>
-        <li class="dropdown">
-            <span>CSS Tutorials</span>
-            <div class="dropdown-content">
-                <a href="./csstutorials/turtlecoders.html">Turtle Coders</a>
-                <a href="./csstutorials/posEx.html">posEx</a>
-                <a href="./csstutorials/floatExBoxes.html">floatExBoxes</a>
-                <a href="./csstutorials/clearEx.html">clearEx</a>
-            </div>
-        </li>
-        <li class="dropdown">
-            <span>Javascript</span>
-            <div class="dropdown-content">
-                <a href="javascript/jsmouse.html">JS Mouse</a>
-                <a href="javascript/jskeyboard.html">JS Keyboard</a>
-            </div>
-        </li>
-        <li class="dropdown">
-            <span>jQuery</span>
-            <div class="dropdown-content">
-                <a href="./jquery/quiz.html">Quiz</a>
-            </div>
-        </li>
-    </ul>
-    <header>
-        <h1 id="aboutme">Michael Bartlett's CSCI445 Website</h1>
-    </header>
     <section>
         <h2>Hello!</h2>
             <h4>Howdy, welcome to my static website for CSCI445! Everything is meticulously tethered together with duplicate code, but at least that's less expense to render!</h4>
---
+++ Web Programming CSCI 445/7 PHP/templateHeader.php
@@ -0,0 +1,29 @@
+<ul id="navbar">
+        <li><a href="javascript:void(0).html" class="active">Home</a></li>
+        <li><a href="aboutme/aboutme.html">About Me</a></li>
+        <li class="dropdown">
+            <span>CSS Tutorials</span>
+            <div class="dropdown-content">
+                <a href="./csstutorials/turtlecoders.html">Turtle Coders</a>
+                <a href="./csstutorials/posEx.html">posEx</a>
+                <a href="./csstutorials/floatExBoxes.html">floatExBoxes</a>
+                <a href="./csstutorials/clearEx.html">clearEx</a>
+            </div>
+        </li>
+        <li class="dropdown">
+            <span>Javascript</span>
+            <div class="dropdown-content">
+                <a href="javascript/jsmouse.html">JS Mouse</a>
+                <a href="javascript/jskeyboard.html">JS Keyboard</a>
+            </div>
+        </li>
+        <li class="dropdown">
+            <span>jQuery</span>
+            <div class="dropdown-content">
+                <a href="./jquery/quiz.html">Quiz</a>
+            </div>
+        </li>
+    </ul>
+    <header>
+        <h1 id="aboutme">Michael Bartlett's CSCI445 Website</h1>
+    </header>
