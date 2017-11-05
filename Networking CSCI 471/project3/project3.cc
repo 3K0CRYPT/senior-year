4908c15345893900f9a0b15c9a2f65fab24914af
Michael Bartlett <bartlettmic@gmail.com>
2017-11-05 12:19:28 -0700

    wew

--- Networking CSCI 471/project3/project3.cc
+++ Networking CSCI 471/project3/project3.cc
@@ -12,7 +12,7 @@ bool seq = false;
 bool expected = true;
 bool ACKed = true;
 char _ack[20] = "ACK                ";
-const int TIMERLENGTH = 20;
+const int TIMERLENGTH = 30;
 
 pkt make_pkt(struct msg message, int seq) {
   struct pkt packet;
