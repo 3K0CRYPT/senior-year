ecf04071c3e8dc7f5a1dcdb378801c08d8cfa57d
Michael Bartlett <bartlettmic@gmail.com>
2017-12-01 12:55:17 -0700

    wew

--- OS 442/mem-sim/src/main.cpp
+++ OS 442/mem-sim/src/main.cpp
@@ -13,68 +13,60 @@
 
 using namespace std;
 
-map<int, Process*> processIDs;
-vector<Process*> processes;
-vector<VirtualAddress> virtualAddresses;
-FlagOptions flags;
+map<int, Process*> processIDs; vector<Process*> processes; vector<VirtualAddress> virtualAddresses; FlagOptions flags;
 
 
 void fileRead(string filename){
-ifstream in;
-in.open(filename);
-int numProcesses;
-string line;
-getline(in, line);
-stringstream ss(line);
-ss >> numProcesses;
-for (int i =0; i < numProcesses; i++){
-	int processId;
-	string processfile;
-	getline(in,line);
-	while(line.empty()){getline(in,line);}
-	stringstream ss1(line);
-	ss1 >> processId >> processfile;
-	ifstream inFile(processfile);
-	istreambuf_iterator<char> eos;
-  	string s(istreambuf_iterator<char>(inFile), eos);
-	istringstream process_stream(s);
-	Process* p = Process::read_from_input(process_stream);
-	processIDs[processId] = p;
-	processes.push_back(p);
-}
-while(getline(in,line)){
-	if(line.empty()) getline(in,line);
-		int pid;
-		string virtAdd;
-		stringstream ss2(line);
-		ss2 >> pid >> virtAdd;
-		VirtualAddress va = VirtualAddress::from_string(pid, virtAdd);
-		virtualAddresses.push_back(va);
-}
+	int processQuantity;
+	string line;
+	stringstream ss(line);
+	ifstream in;
 
+	in.open(filename);
+	getline(in, line);
+	ss >> processQuantity;
+
+	for (int i =0; i < processQuantity; i++) {
+		int processId;
+		string _line;
+		getline(in,line);
+		while(line.empty()){getline(in,line);}
+		stringstream ss1(line);
+		ss1 >> processId >> _line;
+		ifstream inFile(_line);
+		istreambuf_iterator<char> eos;
+		string s(istreambuf_iterator<char>(inFile), eos);
+		istringstream process_stream(s);
+		Process* p = Process::read_from_input(process_stream);
+		processIDs[processId] = p;
+		processes.push_back(p);
+	}
+	while(getline(in,line)){
+		if(line.empty()) getline(in,line);
+		int pid;
+		string virtualAddress;
+		stringstream ss2(line);
+		ss2 >> pid >> virtualAddress;
+		VirtualAddress va = VirtualAddress::from_string(pid, virtualAddress);
+		virtualAddresses.push_back(va);
+		}
 }
 
 void processPrint(){
-map<int,Process*>::iterator it;
-int memAccesses=0;
-int pageFaults=0;
-int usedFrames=0;
-for(it = processIDs.begin(); it != processIDs.end(); it++){
-	
-	cout << "Process " << it->first << "\tMemory Accesses: " << it->second->memory_accesses <<
-		"\tPage Faults: " << it->second->page_faults << "\tFree Frames " << (flags.max_frames - it->second->get_rss()) <<
-		"\tFault Rate: " << it->second->get_fault_percent() <<
-		"\tRSS: " << it->second->get_rss() << endl;
-	memAccesses += it->second->memory_accesses;
-	pageFaults += it->second->page_faults;
-	usedFrames += (it->second->get_rss());
-}
-cout << "TOTAL MEMORY ACCESSES: " << memAccesses <<endl;
-cout << "TOTAL PAGE FAULTS: " << pageFaults <<endl;
+	map<int,Process*>::iterator it;
+	int memAccesses=0, pageFaults=0, usedFrames=0;
+	for(it = processIDs.begin(); it != processIDs.end(); it++){
+		cout << "Process " << it->first << "\tMemory Accesses: " << it->second->memory_accesses <<
+			"\tPage Faults: " << it->second->page_faults << "\tFree Frames " << (flags.max_frames - it->second->get_rss()) <<
+			"\tFault Rate: " << it->second->get_fault_percent() <<
+			"\tRSS: " << it->second->get_rss() << endl;
+		memAccesses += it->second->memory_accesses;
+		pageFaults += it->second->page_faults;
+		usedFrames += (it->second->get_rss());
+	}
+	cout << "TOTAL MEMORY ACCESSES: " << memAccesses <<endl;
+	cout << "TOTAL PAGE FAULTS: " << pageFaults <<endl;
 cout << "TOTAL FREE FRAMES: " << 512 - usedFrames << endl;
-for(int i = 0; i < virtualAddresses.size(); i++){
-	
-}
 }
 
 int main(int argc, char** argv) {
