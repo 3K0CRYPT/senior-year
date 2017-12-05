e9b81be48c03716b6b55d22d1d9c0ff0af1e7bc7
Michael Bartlett <bartlettmic@gmail.com>
2017-12-05 14:32:27 -0700

    wew

--- Networking CSCI 471/project4/resultsC.cc
+++ Networking CSCI 471/project4/resultsC.cc
@@ -38,7 +38,7 @@ void resultsC::displayResults() {
     v4min = len < v4min ? len : v4min;
     v4avg += len;
   }
-  v4avg /= ipv4l.length();
+  v4avg /= ipv4l.size();
   std::cout << "ARPs: " << arp << "\nIPv4: " << ipv4 << ", v4 Max Length:" << v4max << ", v4 Min Length: " << v4min << ", v4 Avg Length: " << v4min << "\nIPv6: " << ipv6 << "\nOther types: " << other << "\nICMP: "<< icmp <<"\nUDP: " << udp << "\nTCP: " << tcp << "\nSYNs: " << syn << "\nFINs: " << fin << "\nFrags: " << frags;
   std::cout << "\nMACs: " << nmac << "\nIPs: " << nip << "\nUDP ports: " << nup << "\nTCP ports: " << ntp << "\nCorrect Checksums: " << checks << std::endl;
 }
