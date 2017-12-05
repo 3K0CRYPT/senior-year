17e19c4afc9936cfe24f13b152975b1cb38f4cb5
Michael Bartlett <bartlettmic@gmail.com>
2017-12-05 14:32:07 -0700

    wew

--- Networking CSCI 471/project4/resultsC.cc
+++ Networking CSCI 471/project4/resultsC.cc
@@ -28,12 +28,17 @@ void resultsC::displayResults() {
   std::cout << "                             \n";
   
   std::cout << "\nA total of " << totalPacketCount << " packets processed." << std::endl;
-  std::cout << "ARPs: " << arp << "\nIPv4: " << ipv4 << "\nIPv6: " << ipv6 << "\nOther types: " << other 
-  << "\nICMP: "<< icmp <<"\nUDP: " << udp << "\nTCP: " << tcp << "\nSYNs: " << syn << "\nFINs: " << fin << "\nFrags: " << frags;
-  int nmac=0, nip=0, nup=0, ntp=0;
+  int nmac=0, nip=0, nup=0, ntp=0, v4max=0,v4min=99999,v4avg=0;
   for (auto p: macs) nmac++;
   for (auto p: ips) nip++;
   for (auto p: udport) nup++;
   for (auto p: tcport) ntp++;
+  for (int len: ipv4l) {
+    v4max = len > v4max ? len : v4max;
+    v4min = len < v4min ? len : v4min;
+    v4avg += len;
+  }
+  v4avg /= ipv4l.length();
+  std::cout << "ARPs: " << arp << "\nIPv4: " << ipv4 << ", v4 Max Length:" << v4max << ", v4 Min Length: " << v4min << ", v4 Avg Length: " << v4min << "\nIPv6: " << ipv6 << "\nOther types: " << other << "\nICMP: "<< icmp <<"\nUDP: " << udp << "\nTCP: " << tcp << "\nSYNs: " << syn << "\nFINs: " << fin << "\nFrags: " << frags;
   std::cout << "\nMACs: " << nmac << "\nIPs: " << nip << "\nUDP ports: " << nup << "\nTCP ports: " << ntp << "\nCorrect Checksums: " << checks << std::endl;
 }
