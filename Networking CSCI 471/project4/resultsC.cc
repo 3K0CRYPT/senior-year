dba46816dfd166772e2a4f79c0f3490b8a29e14d
Michael Bartlett <bartlettmic@gmail.com>
2017-12-05 14:40:48 -0700

    wew

--- Networking CSCI 471/project4/resultsC.cc
+++ Networking CSCI 471/project4/resultsC.cc
@@ -41,6 +41,6 @@ void resultsC::displayResults() {
   v4avg /= ipv4l.size();
   std::cout << "ARPs: " << arp;
   if (arp) std::cout << ", Max Length: 60, Min Length: 60, Avg Length: 60";
-  std::cout << "\nIPv4's: " << ipv4 << ", Max Length: " << v4max << ", Min Length: " << v4min << ", Avg Length: " << v4min << "\nIPv6: " << ipv6 << "\nOther types: " << other << "\nICMP: "<< icmp <<"\nUDP: " << udp << "\nTCP: " << tcp << "\nSYNs: " << syn << "\nFINs: " << fin << "\nFrags: " << frags;
+  std::cout << "\nIPv4's: " << ipv4 << ", Max Length: " << v4max << ", Min Length: " << v4min << ", Avg Length: " << v4min << "\nIPv6: " << ipv6 << "\n Non-TCP/UDP/ICMP types: " << other << "\nICMP: "<< icmp <<"\nUDP: " << udp << "\nTCP: " << tcp << "\nSYNs: " << syn << "\nFINs: " << fin << "\nFrags: " << frags;
   std::cout << "\nMACs: " << nmac << "\nIPs: " << nip << "\nUDP ports: " << nup << "\nTCP ports: " << ntp << "\nCorrect Checksums: " << checks << std::endl;
 }
