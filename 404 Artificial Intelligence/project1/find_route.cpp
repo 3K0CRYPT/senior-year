a145cd54f279183f8dba4ef3b5776ff5fe285a6e
Michael Bartlett <bartlettmic@gmail.com>
2018-01-30 23:36:04 -0700

    nice

--- 404 Artificial Intelligence/project1/find_route.cpp
+++ 404 Artificial Intelligence/project1/find_route.cpp
@@ -7,40 +7,32 @@
 
 using namespace std;
 
-class ComparisonClass
-{
+// So our priority queue knows how to sort things
+class ComparisonClass {
 public:
-  bool operator()(pair<string, int> a, pair<string, int> b)
-  {
-    return a.second < b.second;
-  }
+  bool operator()(pair<string, int> a, pair<string, int> b) { return a.second < b.second; }
 };
 
-typedef map<string, map<string, int>> network;
-typedef priority_queue<pair<string, int>, vector<pair<string, int>>, ComparisonClass> pq;
+typedef map<string, map<string, int> > network;
+typedef priority_queue<pair<string, int>, vector<pair<string, int> >, ComparisonClass> pq;
 
-static int usage(const char *message)
-{
-  cerr << message << endl
-       << "usage: find_route <filename> <origin> <destination>\n";
+static int usage(const char *message) {
+  cerr << message << endl << "usage: find_route <filename> <origin> <destination>\n";
   return 1;
 }
 
-int main(int argc, char *argv[])
-{
-  if (argc != 4)
-    return usage("Improper argument format.");
+int main(int argc, char *argv[]) {
+  if (argc != 4) return usage("Improper argument format.");
   string s = argv[1];
   string origin = argv[2];
   string destination = argv[3];
   ifstream file;
   file.open(argv[1]);
-  if (!file)
-    return usage(("Unable to open \"" + s + '"').c_str());
+  if (!file) return usage(("Unable to open \"" + s + '"').c_str());
 
   network input;
 
-  // Build map network so we can easily construct a graph from it
+  // Build map network so we can easily get cost between 2 cities
   while (file >> s)
   {
     if (s == "END")
@@ -51,33 +43,29 @@ int main(int argc, char *argv[])
     file >> s;
     int cost = stoi(s);
     input[cityA][cityB] = cost;
-    input[cityB][cityA] = cost; //Probably not necessary
+    input[cityB][cityA] = cost;
   }
   file.close();
 
-  if (input.find(origin) == input.end())
-    return usage(("City \"" + origin + "\" not present in input file.").c_str());
-  if (input.find(destination) == input.end())
-    return usage(("City \"" + destination + "\" not present in input file.").c_str());
+  if (input.find(origin) == input.end()) return usage(("City \"" + origin + "\" not present in input file.").c_str());
+  if (input.find(destination) == input.end()) return usage(("City \"" + destination + "\" not present in input file.").c_str());
 
-  map<string, int> start = input[origin];
-  map<string, int> visited;
-  map<string, pair<string, int>> path;
-  pq q;
+  map<string, int> visited;             //Keep track of minimum cost to a city
+  map<string, pair<string, int>> path;  //Keep track of the path between 2 cities
+  pq q;                                 //The priority queue
 
-  q.emplace(make_pair(origin, 0));
+  q.emplace(make_pair(origin, 0));      //Initialize with origin
 
   //The magic!
-  while (!q.empty())
-  {
+  while (!q.empty())  {
     auto city = q.top();
     q.pop();
-    for (auto neighbor : input[city.first])
-    {
+    for (auto neighbor : input[city.first])    {
       int cost_so_far = neighbor.second + city.second;
       auto it = visited.find(neighbor.first);
-      if (it == visited.end() || it->second > cost_so_far)
-      {
+
+      if (it == visited.end() || it->second > cost_so_far)      {
+        //Either we haven't visited this neighbor, or we found a better route to it
         q.emplace(make_pair(neighbor.first, cost_so_far));
         visited[neighbor.first] = cost_so_far;
         path[neighbor.first] = make_pair(city.first, neighbor.second);
@@ -85,9 +73,8 @@ int main(int argc, char *argv[])
     }
   }
 
-  if (visited.find(destination) == visited.end())
-  {
-    cout << "distance: infinity\nroute:\nnone";
+  if (visited.find(destination) == visited.end())  { //We didn't find a route :(
+    cout << "distance: infinity\nroute:\nnone\n";
     return 1;
   }
 
@@ -97,8 +84,7 @@ int main(int argc, char *argv[])
 
   auto current = path[destination];
 
-  while (current.first != origin)
-  {
+  while (current.first != origin)  {
     total_cost += current.second;
     inverted.push_back(current.first);
     current = path[current.first];
@@ -109,10 +95,7 @@ int main(int argc, char *argv[])
   
   cout << "distance: " << total_cost << "km\nroute:\n";
 
-  int i = inverted.size() - 1;
-  for (i; i > 0; i--) {
-    cout << inverted[i] << " to " << inverted[i-1] << ", " << input[inverted[i]][inverted[i-1]] << " km\n";
-  }
+  for (int i = inverted.size() - 1; i > 0; i--)  cout << inverted[i] << " to " << inverted[i-1] << ", " << input[inverted[i]][inverted[i-1]] << " km\n";
 
   return 0;
 };
